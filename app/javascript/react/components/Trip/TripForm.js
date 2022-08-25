f092fc89b0f94eb374ca764d54cb47d54267944f
R-Aranda <rupertaranda@gmail.com>
2022-08-25 10:59:06 -0400

    console log successful trip

--- app/javascript/react/components/Trip/TripForm.js
+++ app/javascript/react/components/Trip/TripForm.js
@@ -31,7 +31,7 @@ const TripForm = () => {
     return makeRequest("/destinations", {
       method: "POST",
       data: values,
-    });
+    }).then((res) => console.log("Trip created successfully", res));
   };
 
   const onSubmit = (values) => {
@@ -65,6 +65,13 @@ const TripForm = () => {
   const options = [{ value: 165, label: "United States" }];
   return (
     <Fragment>
+      {errors?.length > 0 && (
+        <ul className="error">
+          {errors.map((err, i) => {
+            return <li key={i}>{err} was not found</li>;
+          })}
+        </ul>
+      )}
       <Form
         onSubmit={onSubmit}
         mutators={{
