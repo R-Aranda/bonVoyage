948052f618bbc479629687ec61b227fc8b99ca84
R-Aranda <rupertaranda@gmail.com>
2022-10-21 13:55:37 -0400

    refactor context

--- app/javascript/react/contexts/CityContext.js
+++ app/javascript/react/contexts/CityContext.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAsync } from "../hooks/useAsync";
 import { getCity } from "../services/city";
